7fe92298a9508f0426b3e86483dcb6c8685d98b3
Tendai Bhebhe <tendaibhebhe2018@gmail.com>
2023-12-18 10:30:14 +0200

    cleanup creds

--- Geo_Location_Tracker_API/public/init.js
+++ Geo_Location_Tracker_API/public/init.js
@@ -1,11 +1,11 @@
 if (typeof firebase === 'undefined') throw new Error('hosting/init-error: Firebase SDK not detected. You must include it before /__/firebase/init.js')
 ;
 var firebaseConfig = {
-  "projectId": "gpstrackingapi",
-  "appId": "1:849272530588:web:2bf5bdc9ed399e2dffef59",
-  "databaseURL": "https://gpstrackingapi-default-rtdb.firebaseio.com",
-  "apiKey": "AIzaSyB0NY4Q7L1Xf4VtpmH5xkfMXDhPrODPivg",
-  "authDomain": "gpstrackingapi.firebaseapp.com"
+  "projectId": "yourapp",
+  "appId": "yourapp",
+  "databaseURL": "https://yourapp.firebaseio.com",
+  "apiKey": "yourapp",
+  "authDomain": "yourapp.firebaseapp.com"
 };
 
 if (firebaseConfig) {
